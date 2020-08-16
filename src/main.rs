febe740f88a107973da95ca58b794a8afd7a2532
Jon Olin <github.jao@jon.tomolin.net>
2020-08-15 20:42:03 -0400

    Add strength option to bin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::{Arg, App, crate_version};
 use std::error::Error;
+use std::str::FromStr;
 
-use normal_heights::map_normals;
+use normal_heights::map_normals_with_strength;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("Normal Heights")
@@ -14,15 +15,30 @@ fn main() -> Result<(), Box<dyn Error>> {
         .arg(Arg::with_name("OUTPUT")
             .help("Filename to write the normal map to.")
             .required(true))
+        .arg(Arg::with_name("strength")
+            .help("Strength of the resulting normal map.")
+            .next_line_help(true)
+            .short("s")
+            .long("strength")
+            .default_value("6.0")
+            .validator(strength_validator))
         .get_matches();
 
     let input = matches.value_of("INPUT").unwrap();
     let output = matches.value_of("OUTPUT").unwrap();
+    let strength = f32::from_str(matches.value_of("strength").unwrap())?;
 
     let img = image::open(input)?;
 
-    let normal_map = map_normals(&img);
+    let normal_map = map_normals_with_strength(&img, strength);
     normal_map.save(output).unwrap();
 
     Ok(())
+}
+
+fn strength_validator(val: String) -> Result<(), String> {
+    match f32::from_str(&val) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(format!("'{}' is not a valid number. Please use a base 10 number such as '6' or '3.14'.", val)),
+    }
 }
